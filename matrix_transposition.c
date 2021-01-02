5ca3bea88f68f96c9f3a4998bfbb93110fecf911
storm-sergey <st.sergey.a@gmail.com>
2021-01-02 19:34:08 +0300

    ref: codestyling

--- matrix_transposition.c
+++ matrix_transposition.c
@@ -3,10 +3,8 @@
 
 void mtranspose(int **matrix, int **t_matrix, int rows, int cols)
 {
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             t_matrix[j][i] = matrix[i][j];
         }
     }
@@ -23,21 +21,17 @@ int main()
     // mem allocation
     matrix = malloc(rows * sizeof *matrix);
     t_matrix = malloc(cols * sizeof *t_matrix);
-    for (int i = 0; i < rows; i++)
-    {
+    for (int i = 0; i < rows; i++) {
         matrix[i] = malloc(cols * sizeof *matrix[i]);
     }
-    for (int i = 0; i < cols; i++)
-    {
+    for (int i = 0; i < cols; i++) {
         t_matrix[i] = malloc(rows * sizeof *t_matrix[i]);
     }
     
     // filling
     int num = 1;
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             matrix[i][j] = num++;
         }
     }
@@ -46,32 +40,26 @@ int main()
     mtranspose(matrix, t_matrix, rows, cols);
     
     // result printing
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
     printf("\n");
-    for (int i = 0; i < cols; i++)
-    {
-        for (int j = 0; j < rows; j++)
-        {
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < rows; j++) {
             printf("%d ", t_matrix[i][j]);
         }
         printf("\n");
     }
     
     // mem freeing
-    for (int i = 0; i < rows; i++)
-    {
+    for (int i = 0; i < rows; i++) {
         free(matrix[i]);
     }
     free(matrix);
-    for (int i = 0; i < cols; i++)
-    {
+    for (int i = 0; i < cols; i++) {
         free(t_matrix[i]);
     }
     free(t_matrix);
