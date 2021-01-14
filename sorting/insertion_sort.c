8225e0fe60cc4b8492f2d02c9e9e9383e5932808
storm-sergey <st.sergey.a@gmail.com>
2021-01-14 06:35:42 +0300

    Upload insertion_sort.c

---
+++ sorting/insertion_sort.c
@@ -0,0 +1,34 @@
+#include "sorting_test.h"
+
+void insertion_sort(int, int*);
+void swap(int*, int*);
+
+int buff;
+
+int main()
+{
+    sorting_test(insertion_sort);
+    return 0;
+}
+
+void insertion_sort(int n, int *A)
+{
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            if (A[j - 1] > A[j])
+            {
+                swap(&A[j - 1], &A[j]);
+            }
+            else break;
+        }
+    }
+}
+
+void swap(int *a, int *b)
+{
+    buff = *a;
+    *a = *b;
+    *b = buff;
+}
