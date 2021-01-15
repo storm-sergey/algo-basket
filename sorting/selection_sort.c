882e90e69eeba2d3a68c4f30c6cedb14fa2ba86a
storm-sergey <st.sergey.a@gmail.com>
2021-01-15 06:22:02 +0300

    Upload selection_sort.c

---
+++ sorting/selection_sort.c
@@ -0,0 +1,36 @@
+#include "sorting_test.h"
+
+void selection_sort(int, int*);
+void swap(int*, int*);
+
+int buff;
+
+int main()
+{
+    sorting_test(selection_sort);
+    return 0;
+}
+
+void selection_sort(int n, int *A)
+{
+    int min;
+    for (int i = 0; i < n - 1; i++)
+    {
+        min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (A[j] < A[min])
+            {
+                min = j;
+            }
+        }
+        swap(&A[i], &A[min]);
+    }
+}
+
+void swap(int *a, int *b)
+{
+    buff = *a;
+    *a = *b;
+    *b = buff;
+}
