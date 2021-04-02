5dfc334e9c13c5e18ec7345a1558dff4c706f079
storm-sergey <st.sergey.a@gmail.com>
2021-04-02 03:29:47 +0300

    Create binary_search.c

---
+++ binary_search.c
@@ -0,0 +1,31 @@
+// gcc binary_search.c -o b
+
+int main()
+{
+    int x = 4;
+    int* arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    printf("%d", binary_search(x, &arr, 10);
+    return 0;
+}
+
+int binary_search(int x, int* arr, int len)
+{
+    int low = 0;
+    int high = len - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (x < arr[mid])
+        {
+            high = mid - 1;
+        }
+        else if (x > arr[mid])
+        {
+            low = mid + 1;
+        }
+        else if (x == arr[mid])
+        {
+            return mid;
+        }
+    }
+}
