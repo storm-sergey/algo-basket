86dd053bd7c55f77898cf5d45a6bf5261182b52b
storm-sergey <st.sergey.a@gmail.com>
2021-04-07 04:13:03 +0300

    ref: bug fix

--- binary_search.c
+++ binary_search.c
@@ -3,8 +3,8 @@
 int main()
 {
     int x = 4;
-    int* arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    printf("%d", binary_search(x, &arr, 10);
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    printf("%d", binary_search(x, &arr, 10));
     return 0;
 }
 
